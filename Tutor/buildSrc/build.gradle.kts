039be0be73a1ffc39d5b53830af168e086a6ff52
ArindoGhosh <arindoghosh@deloitte.com>
2021-12-01 14:02:21 +0530

    removing commented code

--- Tutor/buildSrc/build.gradle.kts
+++ Tutor/buildSrc/build.gradle.kts
@@ -8,15 +8,3 @@ plugins {
 repositories {
     mavenCentral()
 }
-/*
-buildscript {
-
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-    }
-}*/
